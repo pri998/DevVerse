aa5ded1542b471623c3567fc55110a68475df622
Tideesha Saha <121765715+Tideesha-Saha@users.noreply.github.com>
2025-04-19 13:15:35 +0530

    Update DevVerse.py

--- DevVerse.py
+++ DevVerse.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import PyPDF2
 from Extraction import process_pdf_text
+import time
+from crew_businessAgent import run_business_analyst
+from crew_designAgent import run_design_agent
 
 def local_css(file_name):
     with open(file_name) as f:
@@ -34,12 +37,46 @@ if uploaded_file:
                 requirements, tfidf_vectors = process_pdf_text(pdf_text)
                 st.session_state.requirements = "\n".join(requirements)
 
-                with open("extracted_reqmts.txt", "w", encoding="utf-8") as f:
-                    for req in requirements:
-                        f.write(req + "\n")
-                with st.expander("Extracted Requirements", expanded=False):
-                    if requirements:
-                        for i, req in enumerate(requirements, 1):
-                            st.markdown(f"**{i}.** {req}")
-                    else:
-                        st.warning("No clear requirements found.")
+                # with open("extracted_reqmts.txt", "w", encoding="utf-8") as f:
+                #     for req in requirements:
+                #         f.write(req + "\n")
+                # with st.expander("Extracted Requirements", expanded=False):
+                #     if requirements:
+                #         for i, req in enumerate(requirements, 1):
+                #             st.markdown(f"**{i}.** {req}")
+                #     else:
+                #         st.warning("No clear requirements found.")
+                
+            st.markdown("---")
+
+            #Business Analyst Agent
+            st.subheader("Business Analyst Agent")
+            with st.spinner("Generating Business Analyst Output..."):
+                start_time = time.time()
+                ba_text = run_business_analyst()
+                end_time = time.time()
+                generation_time = end_time - start_time
+            
+                st.markdown(f"Generation time: {generation_time:.2f} seconds.  <span style='color:green;'> Generated Output ✅</span>", unsafe_allow_html=True)
+                ba_output = f'<div style="border: 3px solid #b4d6e3;text-align:left; padding: 20px; border-radius: 5px; color: black; background-color: white; width: 100%;">{ba_text}</div>'
+
+            with st.expander("User Stories", expanded=False):
+                st.subheader("Business Analyst Agent Output:")
+                st.markdown(ba_output,unsafe_allow_html=True)
+
+
+            #Design Agent
+            st.subheader("Design Agent")
+            with st.spinner("Generating Design Agent Output..."):
+                start_time = time.time()
+                # result = crew_d.run_design_agent()
+                design_text = run_design_agent()
+                end_time = time.time()
+                generation_time = end_time - start_time
+            
+                st.markdown(f"Generation time: {generation_time:.2f} seconds.  <span style='color:green;'> Generated Output ✅</span>", unsafe_allow_html=True)
+                da_output = f'<div style="border: 3px solid #b4d6e3; padding: 10px; border-radius: 5px; color: black; background-color: white; width: 100%;">{design_text}</div>'
+
+            with st.expander("System Design and Architecture", expanded=False):
+                st.subheader("Design Agent Output:")
+                st.markdown(da_output, unsafe_allow_html=True)
